68abf91b8900bff65c57a32fad4f4ef7f3303198
johinner <johimadacor@live.com>
2022-08-07 23:29:06 -0500

    agosto 7

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,12 +1,12 @@
 let habilidadMaquina, habilidadAnfitrion;
 let PersonajeAleatorio;
-let vidaMaquina = 3, vidaAnfitrion = 3;
-// typeof(elemento) de que tipo es 
+let vidaMaquina, vidaAnfitrion;
+// typeof(elemento) de que tipo es
 
 const sectionAtaque = document.getElementById("seleccionar-ataque");
 const sectionMascotas = document.getElementById("seleccionar-mascotas");
 const contenedorResultado = document.querySelector(".info-resultado");
-const sectionHabilidades = document.getElementById('section-habilidades');
+const sectionHabilidades = document.getElementById("section-habilidades");
 
 const buttonReiniciar = document.getElementById("reiniciar");
 
@@ -14,178 +14,268 @@ const imagenPersonajeAnfitrio = document.getElementById("personaje-anfitrion");
 const nombrePersonajeAnfitrion = document.getElementById("nombre-personaje");
 
 const imagenPersonajeMaquina = document.getElementById("personaje-maquina");
-const nombrePersonajeMaquina = document.getElementById("nombre-mascota-enemigo");
+const nombrePersonajeMaquina = document.getElementById(
+  "nombre-mascota-enemigo"
+);
 
-const nombreAtaqueAnfitrion = document.getElementById("nombre-ataque-anfitrion");
 const imagenAtaqueAnfitrion = document.getElementById("img-ataque-anfitrion");
 
-const nombreHabilidaMaquina = document.getElementById("nombre-ataque-maquina");
 const imagenHabilidaMaquina = document.getElementById("img-ataque-maquina");
 
 let empates = 0,
   victorias = 0,
   derrotas = 0;
 
+let contadorYamado = 0
+
 const resultados = document.querySelectorAll(".info-resultado span");
 
 const sectionMensaje = document.getElementById("mensaje");
 const resultado = document.getElementById("resultado");
-const contenedorTarjetas = document.getElementById('contenedorTarjetas');
+const contenedorTarjetas = document.getElementById("contenedorTarjetas");
 
 let pers;
+let secuenciaAnfitrion = [];
+let secuenciaMaquina = [];
+let copiaHabilidades = [];
 
 let neverwinter = [];
 let opcionNeverwinter;
 let habilidad;
-class Neverwinter{
-    constructor(nombre, imagen, vida, defensa){
-      this.nombre = nombre;
-      this.imagen = imagen;
-      this.habilidades = [];
-      this.vida = vida;
-      this.defensa = defensa;
-    }
+class Neverwinter {
+  constructor(nombre, imagen, vida, defensa) {
+    this.nombre = nombre;
+    this.imagen = imagen;
+    this.habilidades = [];
+    this.vida = vida;
+    this.defensa = defensa;
+  }
 }
-let guldan =  new Neverwinter("Gul'dan","./assets/Gul'dan.png",55,90,80)
-let tyrande = new Neverwinter('Tyrande', "./assets/Tyrande.png",90,60,50)
-let grommash = new Neverwinter('Grommash',"./assets/Grommash.png",80,70,60)
+let guldan = new Neverwinter("Gul'dan", "./assets/Gul'dan.png", 90, 80);
+let tyrande = new Neverwinter("Tyrande", "./assets/Tyrande.png", 60, 50);
+let grommash = new Neverwinter("Grommash", "./assets/Grommash.png", 70, 60);
 
 guldan.habilidades.push(
-  {nombre: 'Agua', png: './assets/Agua.png', pngEfecto:'./assets/Agua-efecto.png'},
-  {nombre: 'Agua', png: './assets/Agua.png', pngEfecto:'./assets/Agua-efecto.png'},
-  {nombre: 'Agua', png: './assets/Agua.png', pngEfecto:'./assets/Agua-efecto.png'},
-  {nombre: 'Fuego', png: './assets/Fuego.png', pngEfecto:'./assets/Fuego-efecto.png'},
-  {nombre: 'Tierra', png: './assets/Tierra.png', pngEfecto:'./assets/Tierra-efecto.png'}
-)
+  {
+    nombre: "Agua",
+    png: "./assets/Agua.png",
+    pngEfecto: "./assets/Agua-efecto.png",
+  },
+  {
+    nombre: "Agua",
+    png: "./assets/Agua.png",
+    pngEfecto: "./assets/Agua-efecto.png",
+  },
+  {
+    nombre: "Agua",
+    png: "./assets/Agua.png",
+    pngEfecto: "./assets/Agua-efecto.png",
+  },
+  {
+    nombre: "Fuego",
+    png: "./assets/Fuego.png",
+    pngEfecto: "./assets/Fuego-efecto.png",
+  },
+  {
+    nombre: "Tierra",
+    png: "./assets/Tierra.png",
+    pngEfecto: "./assets/Tierra-efecto.png",
+  }
+);
 tyrande.habilidades.push(
-  {nombre: 'Fuego', png: './assets/Fuego.png', pngEfecto:'./assets/Fuego-efecto.png'},
-  {nombre: 'Fuego', png: './assets/Fuego.png', pngEfecto:'./assets/Fuego-efecto.png'},
-  {nombre: 'Fuego', png: './assets/Fuego.png', pngEfecto:'./assets/Fuego-efecto.png'},
-  {nombre: 'Tierra', png: './assets/Tierra.png', pngEfecto:'./assets/Tierra-efecto.png'}
-)
+  {
+    nombre: "Fuego",
+    png: "./assets/Fuego.png",
+    pngEfecto: "./assets/Fuego-efecto.png",
+  },
+  {
+    nombre: "Fuego",
+    png: "./assets/Fuego.png",
+    pngEfecto: "./assets/Fuego-efecto.png",
+  },
+  {
+    nombre: "Fuego",
+    png: "./assets/Fuego.png",
+    pngEfecto: "./assets/Fuego-efecto.png",
+  },
+  {
+    nombre: "Tierra",
+    png: "./assets/Tierra.png",
+    pngEfecto: "./assets/Tierra-efecto.png",
+  },
+  {
+    nombre: "Agua",
+    png: "./assets/Agua.png",
+    pngEfecto: "./assets/Agua-efecto.png",
+  }
+);
 grommash.habilidades.push(
-  {nombre: 'Agua', png: './assets/Agua.png', pngEfecto:'./assets/Agua-efecto.png'},
-  {nombre: 'Fuego', png: './assets/Fuego.png', pngEfecto:'./assets/Fuego-efecto.png'},
-  {nombre: 'Tierra', png: './assets/Tierra.png', pngEfecto:'./assets/Tierra-efecto.png'},
-  {nombre: 'Tierra', png: './assets/Tierra.png', pngEfecto:'./assets/Tierra-efecto.png'},
-  {nombre: 'Tierra', png: './assets/Tierra.png', pngEfecto:'./assets/Tierra-efecto.png'}
-)
+  {
+    nombre: "Agua",
+    png: "./assets/Agua.png",
+    pngEfecto: "./assets/Agua-efecto.png",
+  },
+  {
+    nombre: "Fuego",
+    png: "./assets/Fuego.png",
+    pngEfecto: "./assets/Fuego-efecto.png",
+  },
+  {
+    nombre: "Tierra",
+    png: "./assets/Tierra.png",
+    pngEfecto: "./assets/Tierra-efecto.png",
+  },
+  {
+    nombre: "Tierra",
+    png: "./assets/Tierra.png",
+    pngEfecto: "./assets/Tierra-efecto.png",
+  },
+  {
+    nombre: "Tierra",
+    png: "./assets/Tierra.png",
+    pngEfecto: "./assets/Tierra-efecto.png",
+  }
+);
 
-neverwinter.push(guldan,tyrande,grommash)
-
+neverwinter.push(guldan, tyrande, grommash);
 
 aleatorio = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 iniciarJuego = () => {
-  neverwinter.forEach((neverwinter) =>{
+  neverwinter.forEach((neverwinter) => {
     opcionNeverwinter = `
     <input type="radio" name="neverwinter" id=${neverwinter.nombre} />
     <label class="tarjeta-mokepon" for=${neverwinter.nombre} >
       <img src=${neverwinter.imagen} alt="${neverwinter.nombre}" data-nombre=${neverwinter.nombre}>
       <p>${neverwinter.nombre}</p>
     </label> 
-  `
-  contenedorTarjetas.innerHTML +=  opcionNeverwinter ;
-  })
+  `;
+    contenedorTarjetas.innerHTML += opcionNeverwinter;
+  });
   sectionAtaque.style.display = "none";
   buttonReiniciar.style.display = "none";
   buttonReiniciar.addEventListener("click", reiniciarJuego);
 };
 
 document.addEventListener("click", (e) => {
-  if (e.target.matches(".tarjeta-mokepon img") ) {
+  if (e.target.matches(".tarjeta-mokepon img")) {
     nombrePersonajeAnfitrion.innerText = e.target.dataset.nombre;
     mostrarRutaImagen(e.target.dataset.nombre, imagenPersonajeAnfitrio);
+
     sectionAtaque.style.display = "flex";
     sectionMascotas.style.display = "none";
-    renderizarhabilidades(e.target.dataset.nombre)
+    renderizarhabilidades(e.target.dataset.nombre);
     selecPersonajeMaquina();
   }
-  if(e.target.matches('#section-habilidades img')){
-    nombreAtaqueAnfitrion.innerText = e.target.dataset.nombre
-    mostrarRutaImagen(e.target.dataset.nombre,imagenAtaqueAnfitrion) 
-    selecHabilidadMaquina()
+  if (e.target.matches("#section-habilidades img")) {
+    secuenciaSeleccion(e.target.dataset.nombre);
+    e.target.parentNode.parentNode.textContent = "";
   }
-
 });
 
-selecPersonajeMaquina = () => {
-  PersonajeAleatorio = aleatorio(0, neverwinter.length-1);
-  nombrePersonajeMaquina.innerHTML = neverwinter[PersonajeAleatorio].nombre;
-  imagenPersonajeMaquina.innerHTML = `<img src=${neverwinter[PersonajeAleatorio].imagen} alt="${neverwinter[PersonajeAleatorio].nombre}" />`;
+secuenciaSeleccion = (ataque) => {
+  secuenciaAnfitrion.push(ataque);
+  if (secuenciaAnfitrion.length == 5) {
+    renderizarAtaquesSelecionados(secuenciaAnfitrion, imagenAtaqueAnfitrion);
+    secuenciaAleatoria();
+  }
 };
 
-//   //forEach es el metodo de recorrer un arreglo.
-//   inputMascotas.forEach(function (valor, indice, inputMascotas) {
-//     if (valor.checked == true) {
+secuenciaAleatoria = () => {
+  neverwinter.forEach((datos) => {
+    if (datos.nombre == nombrePersonajeMaquina.textContent) {
+      for (x of datos.habilidades) {
+        secuenciaMaquina.push(x.nombre);
+      }
+      secuenciaMaquina = secuenciaMaquina.sort(() => {
+        return Math.random() - 0.5;
+      });
+    }
+  });
+  renderizarAtaquesSelecionados(secuenciaMaquina, imagenHabilidaMaquina);
+  Enfrenramiento()
+};
+
+renderizarAtaquesSelecionados = (array, id) => {
+  for (x of array) {
+    id.innerHTML += `<img src="./assets/${x}.png" alt="${x}" />`;
+  }
+};
+
+obtenerVidas = (personaje, peticion) => {
+  neverwinter.forEach((datos) => {
+    if (datos.nombre == personaje) {
+      if (peticion == "anfitrion") {
+      } else if (peticion == "maquina") {
+      }
+    }
+  });
+};
+
+selecPersonajeMaquina = () => {
+  PersonajeAleatorio = aleatorio(0, neverwinter.length - 1);
+  nombrePersonajeMaquina.innerHTML = neverwinter[PersonajeAleatorio].nombre;
+  imagenPersonajeMaquina.innerHTML = `<img src=${neverwinter[PersonajeAleatorio].imagen} alt="${neverwinter[PersonajeAleatorio].nombre}" />`;
+  obtenerVidas(neverwinter[PersonajeAleatorio].nombre, "maquina");
+};
 
 renderizarhabilidades = (personaje) => {
   neverwinter.forEach((neverwinter) => {
-    if(personaje == neverwinter.nombre){
-      for(x of neverwinter.habilidades){
+    if (personaje == neverwinter.nombre) {
+      for (x of neverwinter.habilidades) {
         habilidad = `<div>
         <p>${x.nombre}</p>
         <button>
           <img src=${x.png} alt=${x.nombre} data-nombre=${x.nombre}>
         </button>
-      </div>`
-      sectionHabilidades.innerHTML += habilidad;
+      </div>`;
+        sectionHabilidades.innerHTML += habilidad;
       }
     }
-  })
-}
-
-
-// //disable desactiva o activa la funcionalizada de un elemento
-//   if (boolean == false) {
-//     fuego.disabled = false;
-//     agua.disabled = false;
-//     tierra.disabled = false;
-// }
-
+  });
+};
+//ID.checked == true)
+//     ID.disabled = false;
 selecHabilidadMaquina = () => {
-  let longitudArray = neverwinter[PersonajeAleatorio].habilidades.length
-  let AleatorioHabilidad = aleatorio(0, longitudArray-1)
-  let nombre = neverwinter[PersonajeAleatorio].habilidades[AleatorioHabilidad].nombre;
+  let longitudArray = neverwinter[PersonajeAleatorio].habilidades.length;
+  let AleatorioHabilidad = aleatorio(0, longitudArray - 1);
+  let nombre =
+    neverwinter[PersonajeAleatorio].habilidades[AleatorioHabilidad].nombre;
   let png = neverwinter[PersonajeAleatorio].habilidades[AleatorioHabilidad].png;
-  let efecto = neverwinter[PersonajeAleatorio].habilidades[AleatorioHabilidad].pngEfecto;
+  let efecto =
+    neverwinter[PersonajeAleatorio].habilidades[AleatorioHabilidad].pngEfecto;
 
-  nombreHabilidaMaquina.innerText = nombre
-  imagenHabilidaMaquina.innerHTML = `<img src=${png} alt="${nombre}" />`
-
-  // Enfrenramiento();
-  // resultadoFinal();
+  imagenHabilidaMaquina.innerHTML = `<img src=${png} alt="${nombre}" />`;
 };
 
 crearMensaje = (resultado) => {
   let parrafo = document.createElement("div");
-  sectionMensaje.textContent = "";
   parrafo.style.display = "flex";
-  parrafo.innerHTML = `<img src="./assets/${habilidadAnfitrion}-efecto.png" alt="${habilidadAnfitrion}" /> <p>${resultado}</p> <img class="rotate" src="./assets/${habilidadMaquina}-efecto.png" alt="${habilidadMaquina}" />`;
-
+  parrafo.innerHTML = `<img src="./assets/${secuenciaAnfitrion[contadorYamado]}-efecto.png" alt="${secuenciaAnfitrion[contadorYamado]}" /> <p>${resultado}</p> <img class="rotate" src="./assets/${secuenciaMaquina[contadorYamado]}-efecto.png" alt="${secuenciaMaquina[contadorYamado]}" />`;
   sectionMensaje.appendChild(parrafo);
-  vidasRestantes();
+  contadorYamado++
 };
 
 Enfrenramiento = () => {
-  if (habilidadMaquina == habilidadAnfitrion) {
-    crearMensaje("EMPATE");
-    empates++;
-  } else if (
-    (habilidadAnfitrion == "FUEGO" && habilidadMaquina == habilidades[1]) ||
-    (habilidadAnfitrion == "AGUA" && habilidadMaquina == habilidades[0]) ||
-    (habilidadAnfitrion == "TIERRA" && habilidadMaquina == habilidades[2])
-  ) {
-    vidaMaquina = vidaMaquina - 1;
-    crearMensaje("VICTORIA");
-    victorias++;
-  } else {
-    vidaAnfitrion = vidaAnfitrion - 1;
-    crearMensaje("DERROTA");
-    derrotas++;
+  for (let i = 0; i < secuenciaAnfitrion.length; i++) {
+    if (secuenciaMaquina[i] == secuenciaAnfitrion[i]) {
+      crearMensaje("EMPATE");
+      empates++;
+    } else if (
+      (secuenciaMaquina[i] == "Fuego" && secuenciaAnfitrion[i] == "Agua") ||
+      (secuenciaMaquina[i] == "Tierra" && secuenciaAnfitrion[i] == "Fuego") ||
+      (secuenciaMaquina[i] == "Agua" && secuenciaAnfitrion[i] == "Tierra")
+    ){
+      crearMensaje("VICTORIA");
+      victorias++;
+    }else{
+      crearMensaje("DERROTA");
+      derrotas++;
+    }
   }
+  resultadoFinal()
 };
 
 resultadoFinal = () => {
@@ -193,22 +283,15 @@ resultadoFinal = () => {
   resultados[1].innerText = derrotas;
   resultados[2].innerText = empates;
 
-  if (vidaAnfitrion == 0) {
+  if (derrotas > victorias) {
     resultado.innerText = "=> GAME OVER <=";
-    selecHabilidad(true);
-    buttonReiniciar.style.display = "flex";
-    contenedorResultado.style.display = "block";
-  } else if (vidaMaquina == 0) {
+  } else if (victorias > derrotas) {
     resultado.innerText = "=> VIPTORY <=";
-    selecHabilidad(true);
-    buttonReiniciar.style.display = "flex";
-    contenedorResultado.style.display = "block";
+  }else{
+    resultado.innerText = "=> EMPATE <=";
   }
-};
-
-vidasRestantes = () => {
-  document.getElementById("vidad-anfitrion").innerHTML = vidaAnfitrion;
-  document.getElementById("vidad-maquina").innerHTML = vidaMaquina;
+  buttonReiniciar.style.display = "flex";
+  contenedorResultado.style.display = "block";
 };
 
 mostrarRutaImagen = (ruta, id) => {
